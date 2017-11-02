31d549256c501818e2ec8af467731284c1a47ad5
yoann.blot@gmail.com <9G9U0nfMr5Xw>
2017-11-02 11:36:57 +0100

    Add a favicon

--- src/YoannBlot/HouseFinder/View/template.php
+++ src/YoannBlot/HouseFinder/View/template.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title>[[ title ]]</title>
     <link rel="stylesheet" href="/css/home.css"/>
+    <link rel="icon" href="/favicon.ico"/>
 </head>
 <body>
 [[ content ]]
